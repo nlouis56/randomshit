a2e2bb426a86ff1db393ec9fc5fe5a759161a9f5
nlouis56 <nataniel-.louis@epitech.eu>
2023-07-25 14:33:20 +0200

    adding docker full installation script

---
+++ installScripts/full_docker_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
+
+apt-get update
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable docker.service
+systemctl enable containerd.service
+usermod -aG docker $USER
